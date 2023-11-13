a36f13a98df2faa374478150d7b51a28fa3ded24
Seth Doubek <doubekseth@gmail.com>
2023-11-13 13:15:53 -0700

    Messed around with run

--- Custom-Environment/custom-environment/custom_environment_v0.py
+++ Custom-Environment/custom-environment/custom_environment_v0.py
@@ -1,11 +1,17 @@
-from env import graph_layout
+from env import RPS
+import numpy as np
+import numpy.ma as ma
 
-env = graph_layout.parallel_env(render_mode="human")
+env = RPS.parallel_env(render_mode="human")
 observations, infos = env.reset()
 
 while env.agents:
     # this is where you would insert your policy
     actions = {agent: env.action_space(agent).sample() for agent in env.agents}
-
     observations, rewards, terminations, truncations, infos = env.step(actions)
+    print(observations)
+    print(rewards)
+    print(terminations)
+    print(truncations)
+    print(infos)
 env.close()
