b2c519f1f8163dc09913439fd9703ac961dc45e6
Seth Doubek <doubekseth@gmail.com>
2023-11-13 12:40:22 -0700

    Added some of the environment

--- Custom-Environment/custom-environment/custom_environment_v0.py
+++ Custom-Environment/custom-environment/custom_environment_v0.py
@@ -1,1 +1,11 @@
+from env import graph_layout
 
+env = graph_layout.parallel_env(render_mode="human")
+observations, infos = env.reset()
+
+while env.agents:
+    # this is where you would insert your policy
+    actions = {agent: env.action_space(agent).sample() for agent in env.agents}
+
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+env.close()
